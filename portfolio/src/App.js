4b8ff778089129057ce878ed77dbd9bdd4cc2ba7
unknown <leedanleo.91@gmail.com>
2020-03-23 23:43:46 -0700

    Added footer and header.

---
+++ portfolio/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Header from "./components/header";
+import Footer from "./components/footer";
+export default class App extends React.Component{
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />  
+          <Switch>
+            {/* <route path="/" exact component={home}/>
+            <route path="/education" component={education}/>
+            <route path="/projects" component={projects}/>
+            <route path="/workExperience" component={workExperience}/>
+            <route path="/paintGallery" component={workExperience}/>
+            <route path="/activitiesAndVolunteering" component={activitiesAndVolunteering}/> */}
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    );  
+  }
+
+};
+
