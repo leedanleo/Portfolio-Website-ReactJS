8d91e52185e154f931d65285ddc4057b04d365f6
unknown <leedanleo.91@gmail.com>
2020-04-22 02:28:03 -0700

    Got the video to play in the background without the controls appearing when hovering over.

--- portfolio/src/components/background.js
+++ portfolio/src/components/background.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Nature from "./video/waterfall1.mp4";
+import VideoPoster from "./images/GitHub.png"; /*need to change this and make it work later*/
+import './style/background.css';
+
 
 export default class Background extends React.Component {
     render() {
       return (
-            <video controls autoPlay loop muted>
+        <div className="Background-nature">
+            <div id="content">
+              This is where the content wil be
+            </div>
+            <video id="background-nature" poster={VideoPoster} controls autoPlay loop muted>
               <source src={Nature} type="video/mp4" />
             </video>
+        </div>
       );
    }
 }
